02b41872410649963278bb6fed26baf0c207f50c
yorxh14 <jorgeclavijo0607@gmail.com>
2026-09-11 22:03:41 -0300

    Avance 11/09

--- js/app.js
+++ js/app.js
@@ -7,17 +7,28 @@ function MostrarOcultarContraseña() {
         contra.type = "password";
     }
 }
-function ErrorNombre() {
+function Validacion() {
 
     let nombre = document.getElementById("nombreID").value;
-    let mensajeError = document.getElementById("errorCorreo");
-    if (nombre.includes("@gmail.com")) {
+    let contra = document.getElementById("passwordID").value;
+    let mensajeErrorCorreo = document.getElementById("errorCorreo");
+    let mensajeErrorContra = document.getElementById("errorContra");
+    let esValido=false;
+    mensajeErrorContra.innerText= "";
+    mensajeErrorCorreo.innerText= "";
+    if (nombre.includes("@gmail.com") && contra.trim()!="") {
         window.location.href = "index.html";
+       
     } else {
-        mensajeError.innerText = "Escribe un correo válido (@gmail.com)";
-
+        if(!nombre.includes("@gmail.com")){
+            mensajeErrorCorreo.innerText = "Escribe un correo válido (@gmail.com)";
+        }
+        if(contra.trim()==""){
+            mensajeErrorContra.innerText = "La contrasena no puede estar vacia";
+        }
     }
 }
+
 function filtrarCategoria(categoria) {
     let filas = document.querySelectorAll("#productTableBody tr");
     filas.forEach(fila => {
@@ -45,3 +56,35 @@ function buscarProducto() {
 
     });
 }
+
+
+
+const ctx = document.getElementById('migrafico').getContext('2d');
+
+
+const mixedChart = new Chart(ctx, {
+  type: 'bar', 
+  data: {
+    labels: ['8AM', '', '', '12PM', '', '', '4PM', '', '', '8PM'], 
+    datasets: [
+      {
+        type: 'bar', 
+        label: 'facturacion',
+        data: [10, 20, 40, 70, 25, 30, 40, 50, 30, 20], 
+        borderColor: 'rgb(36, 255, 7)',
+        backgroundColor: 'rgb(7, 110, 255)'
+      },
+      
+    ]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+      
+    }
+  }
+});
+
